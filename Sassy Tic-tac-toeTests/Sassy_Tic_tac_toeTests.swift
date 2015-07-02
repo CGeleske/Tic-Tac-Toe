075c7e456337d05e30c80400eb27218a6a7a0de2
Christopher Geleske <Chris@Christophers-MacBook-Pro.local>
2015-07-02 13:48:10 -0500

    Initial Commit

---
+++ Sassy Tic-tac-toeTests/Sassy_Tic_tac_toeTests.swift
@@ -0,0 +1,35 @@
+//
+//  Sassy_Tic_tac_toeTests.swift
+//  Sassy Tic-tac-toeTests
+//
+//  Created by Christopher Geleske on 7/2/15.
+//  Copyright © 2015 Christopher Geleske. All rights reserved.
+//
+
+import XCTest
+
+class Sassy_Tic_tac_toeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
